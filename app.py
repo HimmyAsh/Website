9cadcc944a8a782671248d4ab341285637f85a74
Cameron Ash <94546554+HimmyAsh@users.noreply.github.com>
2023-12-27 15:10:32 -0600

    Fixed CSS misconfiguration

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ st.sidebar.image(Himmy_Ash2, width=175)
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-local_css("style/style.css")
+local_css("styles/style.css")
 
 
 # --Header-- 
